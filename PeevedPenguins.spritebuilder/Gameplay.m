85adc7e60cec499f3d5c0e82db8b25a6b2997c28
cinthiarosa <CINTHIAROSA.rj@gmail.com>
2014-07-09 18:31:57 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -175,6 +175,7 @@
 {
     // the animation manager of each node is stored in the 'animationManager' property
     CCAnimationManager* animationManager = self.animationManager;
+    
     // timelines can be referenced and run by name
     [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
 }
